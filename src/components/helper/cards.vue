8514af7ade562f8651270a4416187f691923f7b1
Alimov503 <shakhalimov5@mail.ru>
2019-06-11 11:46:34 +0500

    to asus

---
+++ src/components/helper/cards.vue
@@ -0,0 +1,83 @@
+<template>
+   <v-container
+    fill-height
+    fluid
+    grid-list-xl
+  >
+       <v-layout >
+      <v-flex
+        sm6
+        xs12
+        md8
+        lg5
+      >
+        <material-stats-card
+          color="green"
+          icon="mdi-store"
+          title="Revenue"
+          value="$34,245"
+          sub-icon="mdi-calendar"
+          sub-text="Last 24 Hours"
+        />
+      </v-flex>
+      <v-flex
+        sm6
+        xs12
+        md8
+        lg5
+      >
+        <material-stats-card
+          color="orange"
+          icon="mdi-content-copy"
+          title="Used Space"
+          value="49/50"
+          small-value="GB"
+          sub-icon="mdi-alert"
+          sub-icon-color="error"
+          sub-text="Get More Space..."
+          sub-text-color="text-primary"
+        />
+      </v-flex>
+      <v-flex
+        sm6
+        xs12
+        md8
+        lg5
+      >
+        <material-stats-card
+          color="red"
+          icon="mdi-information-outline"
+          title="Fixed Issues"
+          value="75"
+          sub-icon="mdi-tag"
+          sub-text="Tracked from Github"
+        />
+      </v-flex>
+      <v-flex
+        sm6
+        xs12
+        md8
+        lg5
+      >
+        <material-stats-card
+          color="info"
+          icon="mdi-twitter"
+          title="Followers"
+          value="+245"
+          sub-icon="mdi-update"
+          sub-text="Just Updated"
+        />
+      </v-flex>
+       </v-layout>
+   </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
